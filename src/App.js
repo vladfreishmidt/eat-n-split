4b3165ef857eb380f0a3f8125b874ce8315f1702
Vladyslav Freishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-09-26 16:53:23 +0300

    Render a list of friends

--- src/App.js
+++ src/App.js
@@ -20,5 +20,48 @@ const initialFriends = [
 ];
 
 export default function App() {
-  return <div>Eat 'n' Split</div>;
+  const friends = initialFriends;
+
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList friends={friends} />
+      </div>
+    </div>
+  );
+}
+
+function FriendsList({ friends }) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <FriendItem friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function FriendItem({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <button className="button">Select</button>
+    </li>
+  );
 }
